906abe4523dcbae751af6279f49fe117b2191af4
knguyen111601 <knguyen111601@gmail.com>
2022-02-16 14:59:05 -0800

    all types and enum

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-let count = 5
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1,2,3];
+let f: any[] = [1, true, "a", false]
 
-count = "a"
+const ColorRed = 0
+const ColorGreen = 1
+const ColorBlue = 2
+
+enum Color { Red, Green, Blue}
+let backgroundColor = Color.Red
